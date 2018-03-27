419ed754d8e7f98c923de36842e50900347f73a0
ronald vilorio <soccer23415@gmail.com>
2018-03-27 18:41:23 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
   resources :students, only: [:index,:show]
-  get '/students/:id/activate', to: 'students#show'
+  get '/students/:id/activate', to: 'students#show', as: 'activate'
 
 
 
