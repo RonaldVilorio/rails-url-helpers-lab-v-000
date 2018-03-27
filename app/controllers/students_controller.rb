ef7f0ee1e7e01b9531a850ab1e23c1c42bb80416
ronald vilorio <soccer23415@gmail.com>
2018-03-27 18:58:01 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,6 +10,8 @@ class StudentsController < ApplicationController
   end
   def activate
     @student = Student.find(params[:id])
+    if @student.active == true
+      @student.active = false
   end
 
   private
