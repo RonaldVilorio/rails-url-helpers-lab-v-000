f186bd589456bd1a66474e520c852349427eb193
ronald vilorio <soccer23415@gmail.com>
2018-03-27 19:06:19 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # However, for the sake of this lab and seeing how you can pass params through
   # the route, we'll keep it explicit for now and refactor later.
 
-  
+
 
   # patch "/activate/:id/activate", to: "students#activate", as: 'activate'
 
