bce90d4a2b81d6ad9694d358a64f4a8c479cf91d
ronald vilorio <soccer23415@gmail.com>
2018-03-27 18:38:46 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :students, only: :edit, controller: :activate
-  resources :students, only: [:index,:show]
+  
+  resources :students, only: [:index,:show,:activate]
 
 
 
